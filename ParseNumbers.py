e824740e64564523393a6f7c26241be6460114e2
Richard Nicholl <nichollri@gmail.com>
2015-12-18 16:26:46 -0500

    RegEx assignemnt for Python Access Web Data

---
+++ ParseNumbers.py
@@ -0,0 +1,35 @@
+import re
+
+# Ctrl - /    <= to comment out lines
+
+
+file_name = 'regex_sum_216719.txt'
+def findnum(text_to_parse):
+    list_of_nums = re.findall('([0-9]+)', text_to_parse)
+    return (list_of_nums)
+
+def get_total(num_list):
+    total = 0
+    for number_str in num_list:
+        number = int(number_str)
+        total = total + number
+    return(total)
+
+#open file and look through lines
+text_file = open(file_name)
+my_total = 0
+for line in text_file:
+    line = line.rstrip()
+    num_list = findnum(line)
+    print num_list
+    my_total += get_total(num_list)
+
+print "Grand total is:", str(my_total)
+
+
+# total = 0
+# for number_str in num_list:
+#     number = int(number_str)
+#     total = total + number
+#
+# print total
